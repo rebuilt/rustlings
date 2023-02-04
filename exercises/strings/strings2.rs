6c93aebc60397c2671ce00455620c4548f93db3d
rebuilt <memoryman51@hotmail.com>
2023-02-04 04:02:01 -0800

    Finish strings exercises

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -2,11 +2,9 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
@@ -14,5 +12,10 @@ fn main() {
 }
 
 fn is_a_color_word(attempt: &str) -> bool {
-    attempt == "green" || attempt == "blue" || attempt == "red"
+    match attempt {
+        "green" => true,
+        "blue" => true,
+        "red" => true,
+        _ => false,
+    }
 }
