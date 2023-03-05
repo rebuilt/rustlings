e2a8ee2043803934c2c8fca8c07eee3e06a5108b
Nelson Jovel <memoryman51@hotmail.com>
2023-03-04 21:08:45 -0800

    Remove mutability from variables that don't need it

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,7 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
-
+#[derive(PartialEq, Debug)]
 pub enum Command {
     Uppercase,
     Trim,
@@ -30,11 +29,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(String::from(string.trim())),
+                Command::Append(num) => {
+                    let mut new_string = string.clone();
+                    for i in 0..*num {
+                        new_string = new_string + "bar";
+                    }
+                    output.push(new_string)
+                }
+            }
         }
         output
     }
@@ -43,12 +53,12 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use my_module;
 
     #[test]
     fn it_works() {
-        let output = transformer(vec![
+        let output = my_module::transformer(vec![
             ("hello".into(), Command::Uppercase),
             (" all roads lead to rome! ".into(), Command::Trim),
             ("foo".into(), Command::Append(1)),
