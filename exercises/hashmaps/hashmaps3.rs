cfcbd71f3a7a9c3b0d517d738fa5c5528f794991
Nelson Jovel <memoryman51@hotmail.com>
2023-03-04 21:08:45 -0800

    Remove mutability from variables that don't need it

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -49,7 +47,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
             team.goals_conceded += team_2_score;
             scores.insert(String::from(&team_1_name), team);
         } else {
-            let mut team1: Team = Team {
+            let team1: Team = Team {
                 name: String::from(&team_1_name),
                 goals_scored: team_1_score,
                 goals_conceded: team_2_score,
@@ -63,7 +61,7 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
             team.goals_conceded += team_1_score;
             scores.insert(String::from(&team_2_name), team);
         } else {
-            let mut team: Team = Team {
+            let team: Team = Team {
                 name: String::from(&team_2_name),
                 goals_scored: team_2_score,
                 goals_conceded: team_1_score,
