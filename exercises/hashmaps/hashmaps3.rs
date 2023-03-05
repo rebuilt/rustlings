cf46908a75c6acf9909f59163431be7c7924bb41
Nelson Jovel <memoryman51@hotmail.com>
2023-03-04 21:04:40 -0800

    Complete hashmap3 exercise

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -19,6 +19,7 @@
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+#[derive(Clone)]
 struct Team {
     name: String,
     goals_scored: u8,
@@ -40,6 +41,35 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        //
+
+        if scores.contains_key(&team_1_name) {
+            let mut team: Team = scores[&team_1_name].clone();
+            team.goals_scored += team_1_score;
+            team.goals_conceded += team_2_score;
+            scores.insert(String::from(&team_1_name), team);
+        } else {
+            let mut team1: Team = Team {
+                name: String::from(&team_1_name),
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            };
+            scores.insert(String::from(&team_1_name), team1);
+        }
+
+        if scores.contains_key(&team_2_name) {
+            let mut team: Team = scores[&team_2_name].clone();
+            team.goals_scored += team_2_score;
+            team.goals_conceded += team_1_score;
+            scores.insert(String::from(&team_2_name), team);
+        } else {
+            let mut team: Team = Team {
+                name: String::from(&team_2_name),
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            };
+            scores.insert(String::from(&team_2_name), team);
+        }
     }
     scores
 }
